e566150e540d53880789c1883814143da44e140c
Mohamede993 <mohamed.e993@outlook.com>
2024-04-08 15:09:34 +0200

    Updates

---
+++ precache-manifest.aa83db1056482c0b213a4561e3b45d5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e678ce4cf5ddebcab9eb62b535ecc340",
+    "url": "/ChatGPTBot/index.html"
+  },
+  {
+    "revision": "a7012c534202928e4783",
+    "url": "/ChatGPTBot/static/css/2.35424416.chunk.css"
+  },
+  {
+    "revision": "a42373f9e6da6e7ea93a",
+    "url": "/ChatGPTBot/static/css/main.0a62645e.chunk.css"
+  },
+  {
+    "revision": "a7012c534202928e4783",
+    "url": "/ChatGPTBot/static/js/2.59a9ebea.chunk.js"
+  },
+  {
+    "revision": "9d0cc3cfc905af4a6940",
+    "url": "/ChatGPTBot/static/js/3.69ba5426.chunk.js"
+  },
+  {
+    "revision": "a42373f9e6da6e7ea93a",
+    "url": "/ChatGPTBot/static/js/main.825a0cce.chunk.js"
+  },
+  {
+    "revision": "74ae5960f1fcb950d5fc",
+    "url": "/ChatGPTBot/static/js/runtime~main.fb1e0e5b.js"
+  }
+]);
